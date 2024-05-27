5c9164a6486b792b7b1da4dccabcabbb0007b449
b410asakura <b410asakura@gmail.com>
2024-05-27 13:38:26 +0600

    add common config and user class

---
+++ src/main/java/com20/task3/enums/Role.java
@@ -0,0 +1,10 @@
+package com20.task3.enums;
+
+public enum Role {
+    ADMIN,
+    USER;
+
+    Role() {
+
+    }
+}
---
+++ src/main/java/com20/task3/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com20.task3.repository;
+
+import com20.task3.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> getUserByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
